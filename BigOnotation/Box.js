81a33405c2b6d3553864a47e63d33aca45b23e30
JosephWamiti <wamiti8711@gmail.com>
2023-08-17 09:14:18 +0300

    O(n)

---
+++ BigOnotation/Box.js
@@ -0,0 +1,13 @@
+function compressBoxesTwice(boxes){
+    boxes.forEach(function(boxes){
+        console.log(boxes);
+    }); 
+
+    boxes.forEach(function(boxes){
+        console.log(boxes)
+    })
+}
+
+// 0(2n)
+//    O(n)
+// O(a + b )
