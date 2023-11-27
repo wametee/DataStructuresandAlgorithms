12b0f69919d7ac37b61826ef6a81980bf0872f66
wamiti8711@gmail.com <wamiti8711@gmail.com>
2023-11-27 07:12:12 +0300

    Feat: skeleton

---
+++ DataStructures/linkedLIst.js
@@ -0,0 +1,10 @@
+// Skeleton
+function SinglyLinkedListNode(data) {
+    this.data = data;
+    this.next = null;
+}
+   
+   function SinglyLinkedList() {
+    this.head = null;
+    this.size = 0;
+}
